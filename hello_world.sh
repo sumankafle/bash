a9d82cea01e4d57395c7ecd090bcc83a5e543722
Suman Kafle <suman.kafle@haw-hamburg.de>
2026-02-02 15:11:24 +0100

    Minimal Hello World output.

---
+++ hello_world.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+###############################################################################
+# Script Name  : hello_world.sh
+# Description  : Minimal Bash script that prints "Hello World!" to standard output.
+#
+# Author       : Suman Kafle
+# Email        : sumankaflee@gmail.com
+# Created Date : 2026-02-02
+#
+# Usage        :
+#   chmod +x hello_world.sh
+#   ./hello_world.sh
+#
+# Exit Codes   :
+#   0  Success
+#
+# Notes        :
+#   - Compatible with Bash 5+
+#   - Uses /usr/bin/env for portability
+###############################################################################
+
+#------------------------------------------------------------------------------
+# Main script logic
+#------------------------------------------------------------------------------
+
+# Print greeting message to stdout
+echo "Hello World!"
